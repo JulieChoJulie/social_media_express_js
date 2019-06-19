8c5f917bb4d1370eb6b44fe7cd552ee96d132dae
JulieChoJulie <h32cho@gmail.com>
2019-06-19 07:55:26 -0400

    twits are added in main pg

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 router.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile', {
@@ -18,12 +19,25 @@ router.get('/join', isNotLoggedIn, (req, res) => {
 });
 
 router.get('/', (req, res) => {
-    res.render('main', {
-        title: 'Main - Express',
-        twits: [],
-        user: req.user,
-        loginError: req.flash('loginError'),
-    });
+    Post.findAll({
+        include: {
+            model: User,
+            attribute: ['id', 'nick'],
+        },
+        order: [['createdAt', 'DESC']],
+    })
+        .then((posts) => {
+            res.render('main', {
+                title: 'Main - Express',
+                twits: posts,
+                user: req.user,
+                loginError: req.flash('loginError'),
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            next(error);
+        });
 });
 
 
