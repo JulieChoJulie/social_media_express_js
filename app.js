3e4bc30abac903835062e33fbc80d2070cb6e568
JulieChoJulie <h32cho@gmail.com>
2019-06-19 00:45:09 -0400

    session secret opt fixed

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.SECRET_KEY));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: process.env.SECRET_KEY,
+    secret: process.env.COOKIE_SECRET,
     cookie: {
         httpOnly: true,
         secure: false,
