7ebcfa46d022256147b1c8a9cf4a02a314fabd18
JulieChoJulie <h32cho@gmail.com>
2019-06-19 07:30:39 -0400

    routes/auth.js added

---
+++ routes/auth.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { User } = require('../models');
+
+const router = express.Router();
+
+router.post('/join', async (req, res, next) => {
+    const { email, nick, password } = req.body;
+    try {
+        const exUser = await User.findOne({ where: { email }});
+        // if the entered email was already in use,
+        if (exUser) {
+            req.flash('joinError', 'The entered email is already in use.');
+            res.redirect('/join');
+        }
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+            email,
+            password: hash,
+            nick,
+        });
+        return res.rendiret('/');
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+    passport.authenticate('local',  (authError, user, info) => {
+        if (authError) {
+            // Error occurred from the server side
+            console.error(authError);
+            return next(authError);
+        }
+        if (!user) {
+            req.flash('loginError', info.message);
+            return res.redirect('/');
+        }
+        return req.login(user, (loginError) => {
+            if (loginError) {
+                console.error(loginError);
+                return next(loginError);
+            }
+            return res.redirect('/');
+        })
+    })(req, res, next); // middleware inside the middleware requires (req, res, next) at the end
+});
+
+router.get('/logout', isLoggedIn, (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+});
+
+module.exports = router;
