cf5141481b5c896afc66d89547d0ef9afccbb367
JulieChoJulie <h32cho@gmail.com>
2019-06-18 22:22:08 -0400

    app.js added

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const morgan = require('morgan');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const pageRouter = require('./routes/page');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8002);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.SECRET_KEY));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SECRET_KEY,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    }
+}));
+app.use(flash());
+
+app.use('/', pageRouter);
+
+// 404 Error Handler
+app.use((req, res, next) => {
+    const error = new Error ('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Listening on port ', app.get('port'));
+});
