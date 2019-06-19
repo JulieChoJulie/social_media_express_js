8ba359ed5f9d24248408bdcfcd390e69a727bf44
JulieChoJulie <h32cho@gmail.com>
2019-06-19 01:04:56 -0400

    user.js added

---
+++ models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, Datatypes) => {
+    sequelize.define('user', {
+        email: {
+            type: Datatypes.STRING(40),
+            allowNull: true,
+            unique: true,
+        },
+        nick: {
+            type: Datatypes.STRING(15),
+            allowNull: false,
+        },
+        password: {
+            type: Datatypes.STRING(100),
+            allowNull: false,
+        },
+        provider: {
+            type: Datatypes.STRING(10),
+            allowNull: false,
+            defaultValue: 'local'
+        },
+        snsId: {
+            type: Datatypes.STRING(30),
+            allowNull: true,
+        },
+    }, {
+        timestamps: true,
+        paranoid: true,
+    })
+};
