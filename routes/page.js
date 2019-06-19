5a056b91c0556d325063ff6232589fb1832d0fad
JulieChoJulie <h32cho@gmail.com>
2019-06-18 22:46:44 -0400

    layout.pug is added

---
+++ routes/page.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/profile', (req, res) => {
+    res.render('profile', {
+        title: 'My Profile - Express',
+        user: null,
+    })
+});
+
+router.get('/join', (req, res) => {
+    res.render('join', {
+        title: 'Join - Express',
+        user: null,
+        joinError: req.flash('joinError'),
+    })
+});
+
+router.get('/', (req, res) => {
+    res.render('join', {
+        title: 'Main - Express',
+        twits: [],
+        user: null,
+        loginError: req.flash('loginError'),
+    });
+});
+
+module.exports = router;
