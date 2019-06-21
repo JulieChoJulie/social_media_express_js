c9c50b9d1671c99ac53229c67af17da4431ae1a8
JulieChoJulie <h32cho@gmail.com>
2019-06-21 00:49:03 -0400

    jwt added

--- social_media_express_API/routes/middlewares.js
+++ social_media_express_API/routes/middlewares.js
@@ -13,3 +13,23 @@ exports.isNotLoggedIn = (req, res, next) => {
         res.redirect('/')
     }
 };
+
+const jwt = require('jsonwebtoken');
+
+exports.verifyToken = (req, res, next) => {
+    try {
+       req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+       return next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(419).json({
+                code: 419,
+                message: 'The token is expired.',
+            });
+        }
+        return res.stauts(401).json({
+            code: 401,
+            message: 'The token is invalid.',
+        });
+    }
+};
