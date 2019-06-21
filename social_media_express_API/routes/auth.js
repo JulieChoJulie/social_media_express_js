5c39cd6763b2a6eae99f492a59c1a597025ab8d3
JulieChoJulie <h32cho@gmail.com>
2019-06-20 23:20:10 -0400

    API server folder

---
+++ social_media_express_API/routes/auth.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { User } = require('../models');
+
+const router = express.Router();
+
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
+    const { email, nick, password } = req.body;
+    try {
+        const exUser = await User.findOne({ where: { email }});
+        // if the entered email was already in use,
+        if (exUser) {
+            req.flash('joinError', 'The entered email is already in use.');
+            res.redirect('/join');
+        }
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+            email,
+            password: hash,
+            nick,
+        });
+        return res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+    passport.authenticate('local',  (authError, user, info) => {
+        if (authError) {
+            // Error occurred from the server side
+            console.error(authError);
+            return next(authError);
+        }
+        if (!user) {
+            req.flash('loginError', info.message);
+            return res.redirect('/');
+        }
+        return req.login(user, (loginError) => {
+            if (loginError) {
+                console.error(loginError);
+                return next(loginError);
+            }
+            return res.redirect('/');
+        })
+    })(req, res, next); // middleware inside the middleware requires (req, res, next) at the end
+});
+
+router.get('/logout', isLoggedIn, (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+});
+
+router.post('/edit', isLoggedIn, async (req, res, next) => {
+    const { email, nick, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { id: req.user.id}});
+        const exUser = await User.findOne({ where: { email }});
+        if (!exUser || exUser.email === user.email){
+            const hash = await bcrypt.hash(password, 12);
+            user.update({
+                email,
+                nick,
+                password: hash
+            });
+            res.redirect('/profile');
+        } else if (exUser){
+            req.flash('editError', 'The entered email is already in use');
+            res.redirect('/edit');
+        }
+
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+module.exports = router;
